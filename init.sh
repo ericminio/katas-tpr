0f17ab27d7a04644a574858039b3ab8947a002a4
ericminio <eric.mignot@gmail.com>
2023-02-27 20:38:29 -0800

    init

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ git branch
 git checkout master
 git reset --hard start
 git checkout start
+git branch
 
 rm -rf donald joe
 mkdir donald
@@ -13,4 +14,18 @@ mkdir joe
 
 cd donald
 pwd
+git clone .. .
+git checkout master
 git branch
+npm install
+
+cd ..
+
+cd joe
+pwd
+git clone .. .
+git checkout master
+git branch
+npm install
+
+cd ..
